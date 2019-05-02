af24dbe92257825a250f1b1c4961d3d2a233183a
Stefan Scepanovic <scepa91@icloud.com>
2019-05-02 16:50:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction("Josh")
+def introduction(Josh)
   puts "Hi, my name is #{name}."
 
 end
 
-def introduction("Josh", "Ember.js")
+def introduction(Josh, Ember.js)
   s  "Hi, my name is #{name} and I am learning to program in #{language}"
 end
